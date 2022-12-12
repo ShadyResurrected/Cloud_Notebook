4a78d0ff247bcb50ff553d73dfadb518ea706f98
ShadyResurrected <cchandra554@gmail.com>
2022-12-12 15:26:04 +0530

    added models and routes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const connectToMongo = require('./db')
-const express = require('express')
+const connectToMongo = require("./db");
+const express = require("express");
+const mongoose = require("mongoose");
 
-connectToMongo()
+mongoose.set("strictQuery", true);
+connectToMongo();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
